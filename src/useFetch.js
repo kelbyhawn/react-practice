56523c68a4a950e00ea4b3e04bbc6e065fcbf449
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2021-12-26 15:24:41 -0800

    add put function to useFetch hook

--- src/useFetch.js
+++ src/useFetch.js
@@ -26,7 +26,7 @@ export default function useFetch(baseUrl) {
   function post(url, body) {
     return new Promise((resolve, reject) => {
       fetch(baseUrl + url, {
-        method: "post",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
@@ -48,5 +48,30 @@ export default function useFetch(baseUrl) {
     });
   }
 
-  return { get, post, loading };
+  function put(url, body) {
+    return new Promise((resolve, reject) => {
+      fetch(baseUrl + url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (!data) {
+            setLoading(false);
+            return reject(data);
+          }
+          setLoading(false);
+          resolve(data);
+        })
+        .catch((error) => {
+          setLoading(false);
+          reject(error);
+        });
+    });
+    }
+
+  return { get, post, put, loading };
 }
