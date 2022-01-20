80f841f862d554726112dadd3312f141630fc7c3
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2022-01-20 12:42:31 -0800

    set document.title based on component

--- src/routes/FetchExamples.js
+++ src/routes/FetchExamples.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import FavColor from "./FavColor";
 import Rain from "./Rain";
 
 export default function FetchExamples() {
+  
+  // Set document title based on component
+  useEffect(() => {
+    document.title = "Fetch Examples";
+  })
+
   return (
     <>
       <h1>Fetch Examples</h1>
