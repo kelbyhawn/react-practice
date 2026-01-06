5110d01c7f6cfd8c4f2ec0a427a08eb93cdcd250
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2026-01-06 13:42:56 -0800

    update to React 18 and Node 22

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import AppWrapper from "./App";
 import "./App.sass";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-      <AppWrapper />
-  </React.StrictMode>,
-  document.querySelector("#root")
+    <AppWrapper />
+  </React.StrictMode>
 );
