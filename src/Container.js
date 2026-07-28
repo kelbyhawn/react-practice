eff7a042877bca81efa5a2e2f12d9c3535fa49cc
kelbyhawn <9256643+kelbyhawn@users.noreply.github.com>
2026-07-28 11:41:22 -0700

    create Header and Footer components

--- src/Container.js
+++ src/Container.js
@@ -1,57 +1,16 @@
 import { NavLink, Outlet } from "react-router-dom";
 import ThemeButton from "./ThemeButton";
+import Header from "./Header";
+import Footer from "./Footer";
 
-// Layout component shares markup you want to show up on every page
-// - like site logo, <nav>, <main>, <footer>, etc...
 export default function Container() {
-  const year = new Date().getFullYear();
-
   return (
     <>
-      <header>
-        {/* Dark/Light mode button */}
-        <ThemeButton />
-
-        {/* Navigation links */}
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Shopping List</NavLink>
-            </li>
-            <li>
-              <NavLink to="/cats">Cats</NavLink>
-            </li>
-            <li>
-              <NavLink to="/fetch-examples">Fetch</NavLink>
-            </li>
-            <li>
-              <NavLink to="/map">Map</NavLink>
-            </li>
-            <li>
-              <NavLink to="/form">Form</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
+      <Header />
       <main>
-        {/* Outlet renders child components linked above into <main> */}
         <Outlet />
       </main>
-
-      {/* Footer */}
-      <footer>
-        <p className="tiny">
-          ©{year} Kelby Hawn •{" "}
-          <a
-            href="https://github.com/kelbyhawn/react-practice"
-            target="_blank"
-            rel="noreferrer"
-          >
-            View source on GitHub
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 }
